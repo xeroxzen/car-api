5a04b18fd669da994181b0a463bce19a398722dd
Google Jr <andilembele020@gmail.com>
2021-04-15 22:25:25 +0200

    setup database

--- carapp/carapp/settings.py
+++ carapp/carapp/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = 'carapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'xeroxzen',
+        'NAME': 'cars',
+        
     }
 }
 
